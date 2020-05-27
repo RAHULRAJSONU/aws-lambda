52f553bab969b8f4c03d857eea15cdb7b17433d6
RAHUL RAJ <RAJRAHUL9939@GMAIL.COM>
2020-05-27 11:01:51 +0530

    Code added to demonstrate environment variable

---
+++ nodejs-app-environment-variables/handler.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports.hello = async event => {
+  return {
+    name: process.env['FIRST_NAME']
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
